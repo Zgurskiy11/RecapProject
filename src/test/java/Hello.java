fbf21afe2e7366209456234fa3dea9bd304dd420
Zgurskiy11 <zgurskiy11@gmail.com>
2021-06-30 17:41:43 -0600

    commit 3

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world" +
                 "");
-        System.out.println("saying more");
+        System.out.println("saying more0lo");
     }
 }
